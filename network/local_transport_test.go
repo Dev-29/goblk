7637335a5ba95f1ba7c48d756204c75e578bd08d
Dev <devabhiram@gmail.com>
2023-11-01 21:33:56 +0530

    Added: TestSendMessage()

--- network/local_transport_test.go
+++ network/local_transport_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// TestNewLocalTransport tests the NewLocalTransport function
 func TestConnect(t *testing.T) {
 	tra := NewLocalTransport("A")
 	trb := NewLocalTransport("B")
@@ -14,4 +15,20 @@ func TestConnect(t *testing.T) {
 	trb.Connect(tra)
 	assert.Equal(t, tra.peers[trb.addr], trb)
 	assert.Equal(t, trb.peers[tra.addr], tra)
+}
+
+// TestSendMessage tests the SendMessage function
+func TestSendMessage(t *testing.T) {
+	tra := NewLocalTransport("A")
+	trb := NewLocalTransport("B")
+
+	tra.Connect(trb)
+	trb.Connect(tra)
+
+	msg := []byte("hello world")
+	assert.Nil(t, tra.SendMessage(trb.addr, msg))
+
+	rpc := <-trb.Consume()
+	assert.Equal(t, rpc.Payload, msg)
+	assert.Equal(t, rpc.From, tra.addr)
 }
